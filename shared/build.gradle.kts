6067dcd1790fe72387cb3388cb9f74e662cdf92c
Baha <ali.bahaabadi@basalam.com>
2024-06-15 02:05:35 +0330

    first commit

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 }
 
 kotlin {
@@ -11,14 +14,18 @@ kotlin {
             }
         }
     }
-    
+
+    val xcframeworkName = "shared"
+    val xcf = XCFramework(xcframeworkName)
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
-            baseName = "shared"
+            baseName = xcframeworkName
+            binaryOption("bundleId", "ir.baha.${xcframeworkName}")
+            xcf.add(this)
             isStatic = true
         }
     }
@@ -29,6 +36,16 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+        val iosMain by creating {
+            dependencies {
+                // Add iOS-specific dependencies here
+            }
+        }
+        val iosTest by creating {
+            dependencies {
+                // Add iOS-specific test dependencies here
+            }
         }
     }
 }
@@ -42,5 +59,49 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["kotlin"])
+
+            groupId = "ir.baha"
+            artifactId = "km"
+            version = "0.1.0"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://github.com/alibahaaa/km")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+}
+
+tasks.register("buildXCFramework") {
+    dependsOn("linkReleaseFrameworkIosArm64", "linkReleaseFrameworkIosX64")
+
+    doLast {
+        val arm64Framework = buildDir.resolve("bin/iosArm64/releaseFramework/shared.framework")
+        val x64Framework = buildDir.resolve("bin/iosX64/releaseFramework/shared.framework")
+        val outputDir = buildDir.resolve("XCFrameworks")
+        val xcFramework = outputDir.resolve("shared.xcframework")
+
+        exec {
+            commandLine = listOf(
+                "xcodebuild",
+                "-create-xcframework",
+                "-framework", arm64Framework.absolutePath,
+                "-framework", x64Framework.absolutePath,
+                "-output", xcFramework.absolutePath
+            )
+        }
     }
 }
